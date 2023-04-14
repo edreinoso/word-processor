d889765481f19a7cfef1bba4ddf34fe36e9d8305
Ed Reinoso <edgardojesus16@gmail.com>
2023-04-14 18:42:46 +0200

    replacing keywords, not working but in the process

--- word.py
+++ word.py
@@ -1,5 +1,8 @@
 import csv
 import re
+import os
+
+temp_file = 'temp.txt'
 
 with open('copy2_overlap.csv', 'r') as csvfile:
     csvreader = csv.reader(csvfile)
@@ -12,14 +15,11 @@ with open('copy2_overlap.csv', 'r') as csvfile:
     
     # Iterate over the remaining rows in the CSV file
     for row in csvreader:
-        """
-         If the last and second to last index of the row match
-            Bingo, there needs to be some computation
-                here needs to 
-        """
+        """ If comparison """
         if (row[3] == '1'):
             print('bingo for word', row[0])
             
+            # Do this for the UN document
             with open('UNParis_A.txt', 'r') as file:
                 keyword = row[0]
                 count = 0
@@ -31,7 +31,24 @@ with open('copy2_overlap.csv', 'r') as csvfile:
                         location = (line_num, match.start())
                         locations.append(location)
                         
-                print(f"The keyword '{keyword}' appears {count} times in the file at the following locations:")
-                for location in locations:
-                    print(f"Line {location[0]}, position {location[1]}")
+                print(f"The keyword '{keyword}' appears {count} times in the file")
+                # print(f"The keyword '{keyword}' appears {count} times in the file at the following locations:")
+                # for location in locations:
+                #     print(f"Line {location[0]}, position {location[1]}")
                 print()
+
+                """ Modify the File """
+                modified_keyword = '🛑' + keyword + '🛑'
+
+                with open(temp_file, 'w') as temp:
+                    for line_num, line in enumerate(file, start=1):
+                        for location in locations:
+                            if location[0] == line_num:
+                                start_pos = location[1]
+                                end_pos = start_pos + len(keyword)
+                                line = line[:start_pos] + modified_keyword + line[end_pos:]
+                        temp.write(line)
+            
+            os.replace(temp_file, 'UNParis_A.txt')
+            # Do this for the Shell document
+    
