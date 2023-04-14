9f6880cb99c127c111678de5720d7e44b7b232b6
Ed Reinoso <edgardojesus16@gmail.com>
2023-04-14 18:23:38 +0200

    changes now with the lines and the position of the words

--- word.py
+++ word.py
@@ -1,4 +1,5 @@
 import csv
+import re
 
 with open('copy2_overlap.csv', 'r') as csvfile:
     csvreader = csv.reader(csvfile)
@@ -18,12 +19,19 @@ with open('copy2_overlap.csv', 'r') as csvfile:
         """
         if (row[3] == '1'):
             print('bingo for word', row[0])
-            keyword = row[0]
-
+            
             with open('UNParis_A.txt', 'r') as file:
+                keyword = row[0]
                 count = 0
-                for line in file:
-                    count += line.count(keyword)
-        
-            print(f"The keyword '{keyword}' appears {count} times in the file.")
-            print()
+                locations = []
+
+                for line_num, line in enumerate(file, start=1):
+                    for match in re.finditer(keyword, line):
+                        count += 1
+                        location = (line_num, match.start())
+                        locations.append(location)
+                        
+                print(f"The keyword '{keyword}' appears {count} times in the file at the following locations:")
+                for location in locations:
+                    print(f"Line {location[0]}, position {location[1]}")
+                print()
