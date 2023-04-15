eb0569d38651b3de8be58e3f6782b0590321801b
Ed Reinoso <edgardojesus16@gmail.com>
2023-04-15 20:29:48 +0200

    modulized code, process file function

--- word.py
+++ word.py
@@ -4,56 +4,68 @@ import os
 
 temp_file = 'temp.txt'
 
-with open('copy2_overlap.csv', 'r') as csvfile:
-    csvreader = csv.reader(csvfile)
-    
-    # Get the header row and store it in a variable
-    header = next(csvreader)
+def process_file(keyword):
+    """ Reading the file """
+    with open('UNParis_A.txt', 'r') as file:
+        count = 0
+        locations = []
 
-    print(header)
-    print()
-    
-    # Iterate over the remaining rows in the CSV file
-    for row in csvreader:
-        """ If comparison """
-        if (row[3] == '1'):
-            print('bingo for word', row[0])
-            
-            """ Reading the file """
-            with open('UNParis_A.txt', 'r') as file:
-                keyword = row[0]
-                count = 0
-                locations = []
-
-                for line_num, line in enumerate(file, start=1):
-                    for match in re.finditer(keyword, line):
-                        count += 1
-                        location = (line_num, match.start())
-                        locations.append(location)
-                        
-                # print(f"The keyword '{keyword}' appears {count} times in the file")
-                print(f"The keyword '{keyword}' appears {count} times in the file at the following locations:")
-                # for location in locations:
-                    # print(location)
-                    # print(f"Line {location[0]}, position {location[1]}")
+        for line_num, line in enumerate(file, start=1):
+            for match in re.finditer(keyword, line):
+                count += 1
+                location = (line_num, match.start())
+                locations.append(location)
                 
-            """ Modify the File """
-            with open('UNParis_A.txt', 'r') as file:
-                modified_keyword = '🛑' + keyword + '🛑'
+        # print(f"The keyword '{keyword}' appears {count} times in the file")
+        print(f"The keyword '{keyword}' appears {count} times in the file at the following locations:")
+        # for location in locations:
+            # print(location)
+            # print(f"Line {location[0]}, position {location[1]}")
+        
+    """ Modify the File """
+    with open('UNParis_A.txt', 'r') as file:
+        modified_keyword = '🛑' + keyword + '🛑'
 
-                with open(temp_file, 'w') as temp:
-                    
-                    for line_num, line in enumerate(file, start=1):
-                        for location in locations:
-                            if location[0] == line_num:
-                                start_pos = location[1]
-                                end_pos = start_pos + len(keyword)
-                                line = line[:start_pos] + modified_keyword + line[end_pos:]
-                        temp.write(line)
-
-            os.replace(temp_file, 'UNParis_A.txt')
+        with open(temp_file, 'w') as temp:
             
-            print()
+            for line_num, line in enumerate(file, start=1):
+                for location in locations:
+                    if location[0] == line_num:
+                        start_pos = location[1]
+                        end_pos = start_pos + len(keyword)
+                        line = line[:start_pos] + modified_keyword + line[end_pos:]
+                temp.write(line)
+
+    os.replace(temp_file, 'UNParis_A.txt')
+    pass
+
+def main():
+    counter = 0
+    with open('copy2_overlap.csv', 'r') as csvfile:
+        csvreader = csv.reader(csvfile)
+        
+        # Get the header row and store it in a variable
+        header = next(csvreader)
+
+        print(header)
+        print()
+        
+        # Iterate over the remaining rows in the CSV file
+        for row in csvreader:
+            counter += 1
+            """ If comparison """
+            # if (row[2+counter] == '1'): # UN_Overlap_Shell
+            if (row[3] == '1'): # UN_Overlap_Shell
+                print('bingo for word', row[0])
+                
+                process_file(row[0])
+
+                print()
+
+if __name__ == "__main__":
+    main()
+
+
     
     
     """Do this for the Shell document"""
