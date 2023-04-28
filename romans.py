4e99c365b0bb94f705a5c7aa177c0fcec5eae74c
Ed Reinoso <edgardojesus16@gmail.com>
2023-04-28 17:25:35 +0200

    some edits were done to the word count

--- romans.py
+++ romans.py
@@ -1,7 +1,9 @@
 import re
 
-list_of_words = ['This', 'document', 'introduction']
-
+list_of_words = ['Parties',
+    'Agreement',
+    'Article',
+    'Conference']
 # function to convert Roman numeral to decimal
 def roman_to_decimal(roman_num):
     roman_dict = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
@@ -25,36 +27,42 @@ with open('./files/UNParis_A_romans.txt', 'r') as f:
         line = f.readline()
         
         if not line:
-            print('🚧 Total num of words', total_num_words, 'absolute count (nouns)', absolute_count, 'relative count', relative_count)
+            print('🚧 Total num of words', total_num_words, '\nabsolute count (nouns)', absolute_count, 'relative count', relative_count)
             break
-        # print(line)
 
         # check if line contains a Roman numeral
-        roman_num_match = re.search('[IVXLCDM]+', line)
+        roman_num_match = re.search(r"\b[IVXLCDM]+\.", line)
+        # roman_num_match = re.search('[IVXLCDM]+', line)
         # print(roman_num_match)
+        # if roman_num_match:
+            # roman_num = roman_num_match.group(0)
+            # print('hello',roman_num)
+        # else:
+            # print('world')
+        
         if roman_num_match:
             # convert Roman numeral to decimal and update current section
             roman_num = roman_num_match.group(0)
-            decimal_num = roman_to_decimal(roman_num)
-            current_section = decimal_num
+            # decimal_num = roman_to_decimal(roman_num)
+            # current_section = decimal_num
             # I want to be able to output the computation that happened below
             if total_num_words != 0:
                 relative_count = absolute_count / total_num_words
-                print('🚧 Total num of words', total_num_words, 'absolute count (nouns)', absolute_count, 'relative count', relative_count)
+                print('🚧 Total num of words', total_num_words, '\nabsolute count (nouns)', absolute_count, 'relative count', relative_count)
             # print('🚧 New section')
-            
+            print(roman_num)
             # and then reset the counters, cause this is just bunch of counters
             absolute_count = 0
             relative_count = 0
             total_num_words = 0
         else:
             # compute something based on current section and current line
-            if current_section is not None:
-                # do computation here based on current_section and current line
-                # mainly I would like to have the relative word count and the absolute word count
-                line_words = len(line.split())
-                total_num_words += line_words
-                for word in list_of_words:
-                    if word in line:
-                        absolute_count += 1
-                # print(f"Section {current_section}: {line} Word count {line_words}")
+            # if current_section is not None:
+            # do computation here based on current_section and current line
+            # mainly I would like to have the relative word count and the absolute word count
+            line_words = len(line.split())
+            total_num_words += line_words
+            for word in list_of_words:
+                if word in line:
+                    absolute_count += 1
+            # print(f"Section {current_section}: {line} Word count {line_words}")
