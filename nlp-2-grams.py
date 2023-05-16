4a02aa517bdc8dd8b328bcde8f9cb66981f9f21d
Ed Reinoso <edgardojesus16@gmail.com>
2023-05-16 17:41:42 +0200

    running test with the entire file

--- nlp-2-grams.py
+++ nlp-2-grams.py
@@ -1,8 +1,12 @@
 import re
+import os
+import csv
 import nltk
 from collections import Counter
 from nltk import ngrams
 from nltk.corpus import stopwords
+from nltk.tokenize import sent_tokenize
+from datetime import datetime
 stop_words = set(stopwords.words('english'))
 
 sentence = """The promotion of road safety awareness among
@@ -29,38 +33,57 @@ Safe to Home” programme focuses on helping
 children to travel safely to and from school.
 (See more on our road safety work on page 32)."""
 
+in_directory = '/Users/elchoco/clients/oldrich/files/input/small_set'
+out_directory = '/Users/elchoco/clients/oldrich/files/csv/output/'
+tag = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
+
 n = 2
 
-wordsList = [ w for w in sentence if not w in stop_words ]
+for filename in os.listdir(in_directory):
+    if filename.endswith('.txt'):
+        with open(in_directory+'/'+filename, 'r') as infile:
+            txt = infile.read()
+            txt_lower = txt.lower() # lower case the document
+            print(type(txt_lower))
 
-# Using a Tagger. Which is part-of-speech
-# tagger or POS-tagger.
-tagged = nltk.pos_tag(wordsList)
+            # tokenized = sent_tokenize(txt_lower)
 
-twograms = ngrams(sentence.split(), n)
+            # for i in tokenized:
+            # wordsList = nltk.word_tokenize(i)
 
-twoeuros = list(twograms)
+            wordsList = [ w for w in txt_lower if not w in stop_words ]
+            
+            # print(type(wordsList))
+            
+            # Using a Tagger. Which is part-of-speech
+            # tagger or POS-tagger.
+            tagged = nltk.pos_tag(wordsList)
+            
+            # twograms = ngrams(wordsList.split(), n)
+            twograms = ngrams(txt_lower.split(), n)
 
-# removing special characters
-# would probably have to test whether this may be accurate 
-pattern = r'[^\w\s]'
-
-cleaned_list_nested = []
-for tpl in twoeuros:
-    cleaned_tpl = []
-    for element in tpl:
-        cleaned_element = re.sub(pattern, '', element)
-        cleaned_tpl.append(cleaned_element)
-    cleaned_list_nested.append(tuple(cleaned_tpl))
-# print(cleaned_list_nested)
-
-# print(twoeuros)
-
-# frequency
-twofreq = Counter(cleaned_list_nested)
-# twofreq = Counter(twoeuros)
-
-print(twofreq)
-
-# for grams in sixgrams:
-#   print(grams, type(grams))
+            twoeuros = list(twograms)
+            
+            # removing special characters
+            # would probably have to test whether this may be accurate 
+            pattern = r'[^\w\s]'
+            cleaned_list_nested = []
+            for tpl in twoeuros:
+                cleaned_tpl = []
+                for element in tpl:
+                    cleaned_element = re.sub(pattern, '', element)
+                    cleaned_tpl.append(cleaned_element)
+                cleaned_list_nested.append(tuple(cleaned_tpl))
+            # print(twoeuros)
+            
+            # frequency
+            # twofreq = Counter(cleaned_list_nested)
+            twofreq = Counter(twoeuros)
+            # print(twofreq)
+            
+            with open(out_directory+tag+"_"+filename+".csv", 'w', newline='') as csvfile:
+                        writer = csv.writer(csvfile)
+                        writer.writerow(['Word', 'Frequency'])
+                        for word, freq in twofreq.most_common():
+                            mystring = ' '.join(word)
+                            writer.writerow([mystring,freq])
