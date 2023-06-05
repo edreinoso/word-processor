f4c2f1ba96e96a926b3ba3d6dfd0a9616c886f10
Ed Reinoso <edgardojesus16@gmail.com>
2023-06-05 09:55:23 +0200

    changed the approach for the emojis

--- word.py
+++ word.py
@@ -1,126 +1,72 @@
 import csv
-import re
 import os
 
-local_word_list = []
+in_directory = '/Users/elchoco/clients/oldrich/files'
+temp_file = "file.txt"
 
-document_dict = {
-    'paris': 'UNParis_A.txt',
-    'bp':'BP_2015.txt',
-    'shell':'Shell_2015.txt'
-}
+paris_agreement_path = '/Users/elchoco/clients/oldrich/files/PA.txt'
+paris_rulebook_path = '/Users/elchoco/clients/oldrich/files/PR.txt'
+glasgow_climate_path = '/Users/elchoco/clients/oldrich/files/GCP.txt'
 
-def process_file(word, frequency, file_name, emoji_type):
-    # print(word, frequency, file_name, emoji_type)
-    value = document_dict[file_name.lower()]
-    list_docs = [document_dict['paris'], value]
-    # list_docs = [value]
-    
-    for docs in list_docs:
-        if docs == 'UNParis_A.txt' and word in local_word_list:
-            # print('*** word has already been added',docs,word, frequency, file_name)
-            continue
-        
-        else:
-            print('new word',docs,word, frequency, file_name)
-            local_word_list.append(word)
-            """ Reading from the file """
-            with open('./files/'+docs, 'r') as file:
-                count = 0
-                locations = []
+with open(paris_agreement_path, 'r') as file:
+    paris_agreement_text = file.read()
+with open(paris_rulebook_path, 'r') as file:
+    paris_rulebook_text = file.read()
+with open(glasgow_climate_path, 'r') as file:
+    glasgow_climate_text = file.read()
 
-                for line_num, line in enumerate(file, start=1):
-                    for match in re.finditer(word, line):
-                        count += 1
-                        location = (line_num, match.start())
-                        locations.append(location)
-                        
-                # print(f"The word '{word}' appears {count} times in the file")
-                print(f"The word '{word}' appears {count} times in the file at the following locations:")
-                # for location in locations:
-                    # print(location)
-                    # print(f"Line {location[0]}, position {location[1]}")
-            
-            """ Writing to the file """
-            temp_file = 'temp.txt'
-            with open('./files/'+docs, 'r') as file: # I have to open the file again, otherwise there would be an error.
-                modified_word = emoji_type + frequency + "-" + word + "-" + frequency + emoji_type
-                with open(temp_file, 'w') as temp:
-                    # # If I don't open the file again, there would not be any reading
-                    for line_num, line in enumerate(file, start=1):
-                        for location in locations:
-                            if location[0] == line_num:
-                                start_pos = location[1]
-                                end_pos = start_pos + len(word)
-                                line = line[:start_pos] + modified_word + line[end_pos:]
-                        temp.write(line)
-            os.replace(temp_file, './files/'+docs)
-
-def row_evaluation(matrix):
-    foo = []
-    for i in range(1, len(matrix)): # row
-        compared = True
-        # print('----row', matrix[i][0]) # sanity check
-        
-        for j in range(3, len(matrix[0])): # col
-            # print('****column',matrix[i][j])
-            if matrix[i][j] != '1':
-                compared = False
-                continue
-
-        if compared:
-            foo.append(True)
-        else:
-            foo.append(False)
-        
-        # print(compared)
-        # print()
-        
-    return foo
+def insert_emojis(text, word, frequency, emoji):
+    modified_text = text.replace(word, emoji + '-' + frequency + '-' + word + '-' + frequency + '-' + emoji)
+    return modified_text
 
 def main():
-    tp1_emojis_0 = '⚠️'
-    tp1_emojis_1 = '🛑'
-    tp2_emojis_0 = '💩'
-    tp2_emojis_1 = '💶'
+    pa_emoji = paris_agreement_text
+    pr_emoji = paris_rulebook_text
+    gcp_emoji = glasgow_climate_text
+    for filename in os.listdir(in_directory):
+	    if filename.endswith('.csv'):
+                with open(in_directory+'/'+filename, 'r') as file:
+                    reader = csv.reader(file)
+                    matrix = []
 
-    with open('copy2_overlap.csv', 'r') as file:
-        reader = csv.reader(file)
-        matrix = []
-        foo = []
+                    for row in reader: 
+                        matrix.append(row) # creating a matrix
+                    
+                    # Iterate through columns first, then rows
+                    for j in range(1,len(matrix[0])):
+                        doc_name = matrix[0][j].split(" ")[1]
+                        # print('###',doc_name)
+                        """if doc_name == 'PA':
+                            main_text = paris_agreement_text
+                        elif doc_name == 'PR':
+                            main_text = paris_rulebook_text
+                        else:
+                            main_text = glasgow_climate_text"""
+                        
+                        for i in range(1,len(matrix)):
+                            word = matrix[i][0]
+                            frequency = matrix[i][1]
+                            
+                            if filename == 'sheet1.csv':
+                                emoji = '🛑'
+                            else: 
+                                emoji = '🟢'
+                            
+                            # main_text = insert_emojis(main_text, word, frequency, emoji)
+                            if doc_name == 'PA':
+                                pa_emoji = insert_emojis(pa_emoji, word, frequency, emoji)
+                            elif doc_name == 'PR':
+                                pr_emoji =  insert_emojis(pr_emoji, word, frequency, emoji)
+                            else:
+                                gcp_emoji = insert_emojis(gcp_emoji, word, frequency, emoji)
 
-        for row in reader: 
-            matrix.append(row) # creating a matrix
-
-        # computing whether the values of each of the row is true or false.
-        foo = row_evaluation(matrix)
-        print(foo)
-
-        # Iterate through columns first, then rows
-        for j in range(3,len(matrix[0])):
-            file_name = matrix[0][j].split("_")[-1]
-            print('----col', matrix[0][j]) # sanity check
-            count = 0 # inner count for foo
-            for i in range(1,len(matrix)):
-                word = matrix[i][0]
-                frequency = matrix[i][1]
-                tp_word = matrix[i][2]
-
-                if tp_word == 'Noun': # separating then by noun or 2-grams
-                    # print('i:',i,'word:',matrix[i][0])
-                    if matrix[i][j] == '1' and foo[count]: # if I'm one, and the rest is also one, then do
-                        # print('match',matrix[i][j])
-                        process_file(word, frequency, file_name, tp1_emojis_1)
-                    else: # if I'm one, but others are not 1
-                        process_file(word, frequency, file_name, tp1_emojis_0)
-                else:
-                    if matrix[i][j] == '1' and foo[count]: # if I'm one, and the rest is also one, then do
-                        process_file(word, frequency, file_name, tp2_emojis_1)
-                    else: # if I'm one, but others are not 1
-                        process_file(word, frequency, file_name, tp2_emojis_0)
-
-                count += 1
-            print()
+    # Write the main_text into a file
+    with open(in_directory+'/out/pa.txt', 'w') as output_file:
+        output_file.write(pa_emoji)
+    with open(in_directory+'/out/pr.txt', 'w') as output_file:
+        output_file.write(pr_emoji)
+    with open(in_directory+'/out/gcp.txt', 'w') as output_file:
+        output_file.write(gcp_emoji)
 
 if __name__ == "__main__":
     main()
